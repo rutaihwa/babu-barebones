f4664b14efbf0bc8f046dbb6a0b401c63476b629
Rutaihwa <rutaihwa@gmail.com>
2015-11-29 07:48:11 +0300

    Rerun tests whenever there is a file change

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Gulp = require('gulp');
+var Exec = require('child_process').exec;
+
+// Run tests
+Gulp.task('test', function (callback) {
+
+    Exec('npm test', function (error, stdout, stderr) {
+
+        console.log(stdout);
+        console.log(stderr);
+        callback();
+    });
+});
+
+// Run installs
+Gulp.task('modules', function (callback) {
+
+    Exec('npm install', function (error, stdout, stderr){
+
+        console.log(stdout);
+        console.log(stderr);
+        callback(error);
+    });
+});
+
+// Watch changes
+Gulp.task('watch', ['modules', 'test'], function () {
+
+    // If files changes run test coverages
+    Gulp.watch(['lib/**/*.js', 'test/**/**/*.js'], ['test']);
+
+    // If package.json changes install packages
+    Gulp.watch(['package.json'], ['modules']);
+});
